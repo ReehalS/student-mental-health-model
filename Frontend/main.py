24dff5994f55c0904b5e2197f29390cfd5a3e186
reehals <sunny.reehal@gmail.com>
2024-12-10 22:10:59 -0800

    Update frontend

--- Frontend/main.py
+++ Frontend/main.py
@@ -3,6 +3,47 @@ import joblib
 import pandas as pd
 import os
 
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.regularizers import l1, l2
+from tensorflow.keras.optimizers import SGD
+
+from scikeras.wrappers import KerasClassifier
+from sklearn.model_selection import GridSearchCV, KFold
+
+N_FEATURES = 4
+BINARY_CLASSIFICATION= True
+N_CLASSES = 5
+# add confusion matrix, f1 + auc score, precision/recall + roc curves
+# Define build_model function for KerasClassifier
+# def build_model(num_layers, num_neurons, activation, dropout_rate, momentum, reg_method, reg_rate, learning_rate):
+def build_model(num_layers, num_neurons, activation, momentum, reg_method, reg_rate, learning_rate):
+    regularizer = l1(reg_rate) if reg_method=='l1' else l2(reg_rate)
+
+    model = Sequential()
+
+    # Input layer
+    model.add(Input((N_FEATURES,)))
+    # Hidden layers
+    for _ in range(num_layers):
+        model.add(Dense(num_neurons, activation=activation, kernel_regularizer=regularizer))
+        # model.add(Dropout(dropout_rate))
+    # Output layer
+    if BINARY_CLASSIFICATION:
+        model.add(Dense(1, activation='sigmoid')) # binary classification
+    else:
+        model.add(Dense(N_CLASSES, activation='softmax')) # multi-class classification
+
+    optimizer = SGD(learning_rate=learning_rate, momentum=momentum)
+    loss_string = 'binary_crossentropy' if BINARY_CLASSIFICATION else 'sparse_categorical_crossentropy'
+    model.compile(loss=loss_string, optimizer=optimizer, metrics=['accuracy'])
+    return model
+
 # Define input features and their constraints
 st.title("Depression Prediction")
 
@@ -10,13 +51,33 @@ st.title("Depression Prediction")
 age = st.number_input("Age", min_value=16, max_value=35, step=1, format="%d")
 cgpa = st.number_input("CGPA", min_value=0.0, max_value=4.0, step=0.01, format="%.2f")
 semester_credit_load = st.number_input("Semester Credit Load", min_value=10, step=1, format="%d")
-anxiety_score = st.slider("Anxiety Score", min_value=1, max_value=5, step=1)
 
 major = st.selectbox("Major", ["business", "engineering", "law", "medical", "others"])
 
+if(major=="business"):
+    modelName="Naive Bayes"
+    modelType= "GaussianNB"
+elif(major=="engineering"):
+    modelName="Support Vector Machine"
+    modelType="SVC"
+elif(major=="law"):
+    modelName="Naive Bayes"
+    modelType="GaussianNB"
+elif(major=="medical"):
+    modelName="Neural Network"
+    modelType="KerasClassifier"
+elif(major=="others"):
+    modelName="K-Nearest Neigbors Classifier"
+    modelType="KNeighborsClassifier"
+else:
+    modelName="Error"
+    modelType="Error"
+
 # Load model parameters based on selected major
-model_params_path = f"Frontend/logistic_regression_{major}_model.pkl"
-scaler_params_path = f"Frontend/logistic_regression_{major}_scaler.pkl"
+model_params_path = f"Frontend/{modelType}_{major}_model.pkl"
+scaler_params_path = f"Frontend/{modelType}_{major}_scaler.pkl"
+
+st.text(f"Model used for {major} major is {modelName}")
 
 if os.path.exists(model_params_path) and os.path.exists(scaler_params_path):
     with open(model_params_path, 'rb') as model_file:
@@ -29,16 +90,12 @@ else:
 
 # Predict button
 if st.button("Predict"):
-    input_features = [[age, cgpa, anxiety_score, semester_credit_load]]
-    feature_names = ["Age", "CGPA", "Anxiety_Score", "Semester_Credit_Load"]
+    input_features = [[age, cgpa, semester_credit_load]]
+    feature_names = ["Age", "CGPA", "Semester_Credit_Load"]
 
-    # Convert to DataFrame with feature names
     input_df = pd.DataFrame(input_features, columns=feature_names)
-
-    # Scale input features
     scaled_features = scaler.transform(input_df)
 
-    # Make prediction
     prediction = model.predict(scaled_features)[0]
 
     if prediction == 0:
